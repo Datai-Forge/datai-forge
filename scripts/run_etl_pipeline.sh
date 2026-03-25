bd6e32dca8eb5e01c8529bf8660ba15b2afca99d
Nouster <djebali.mohamed88@gmail.com>
2026-03-25 08:04:28 +0000

    feat: data processing for security silver layer

--- scripts/run_etl_pipeline.sh
+++ scripts/run_etl_pipeline.sh
@@ -9,6 +9,7 @@ python3 -m src.etl.bronze.bronze_securite
 
 echo "2/3 Transformation Silver..."
 python3 -m src.etl.silver.silver_presidentielle
+python3 -m src.etl.silver.silver_securite
 
 echo "3/3 Transformation Gold..."
 python3 -m src.etl.gold.gold_presidentielle_bi
